b470ae6a2d8fd74ea25118d0e08bd47e1758d539
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-28 10:24:55 +0530

    boring apartments

---
+++ IMPLEMENTATION/24_1433_A_boring_apartments.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#define ll long long int
+#define cint \
+    int t;   \
+    cin >> t;
+#define br << endl;
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout << fixed << setprecision(n);
+#define For(i, n) for (ll i = 0; i < n; i++)
+#define Forran(i, n, a) for (ll i = n + 1; i <= a; i++)
+#define Forrevran(i, n, a) for (ll i = n - 1; i >= a; i--)
+#define Forrev(i, n) for (ll i = n - 1; i >= 0; i--)
+#define total(v) accumulate(v.begin(), v.end(), 0)
+#define diff(ax, ay, bx, by) abs(ax - bx) + abs(ay - by)
+#define distance(ax, ay, bx, by) pow((ax - bx) * (ax - bx) + (ay - by) * (ay - by), 0.5)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        string n;
+        cin >> n;
+        
+        int number = stoi(n);
+        int press = 0;
+        
+        for(int digit = 1; digit <= 9; digit++) {
+            string current = "";
+            
+            for(int len = 1; len <= 4; len++) {
+                current += to_string(digit);  
+                press += len;
+                
+                if(current == n) {
+                    cout << press << endl;
+                    goto end_of_loop;
+                }
+            }
+        }
+        end_of_loop:;
+    }
+ 
+    return 0;
+}
