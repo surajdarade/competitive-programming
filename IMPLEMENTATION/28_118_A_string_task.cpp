5ad5342eb2e278dbddae5f5927bea5a7dde2b8d8
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-29 08:42:02 +0530

    string task

---
+++ IMPLEMENTATION/28_118_A_string_task.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#define ll long long int
+#define cint \
+    int t;   \
+    cin >> t;
+#define br << endl;
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout << fixed << setprecision(n);
+#define For(i, n) for (ll i = 0; i < n; i++)
+#define Forran(i, n, a) for (ll i = n + 1; i <= a; i++)
+#define Forrevran(i, n, a) for (ll i = n - 1; i >= a; i--)
+#define Forrev(i, n) for (ll i = n - 1; i >= 0; i--)
+#define total(v) accumulate(v.begin(), v.end(), 0)
+#define diff(ax, ay, bx, by) abs(ax - bx) + abs(ay - by)
+#define distance(ax, ay, bx, by) pow((ax - bx) * (ax - bx) + (ay - by) * (ay - by), 0.5)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    string s;
+    cin >> s;
+    
+    string vowels = "aoyeuiAOYEUI";
+    string result = "";
+ 
+    for (char c : s) {
+        c = tolower(c);
+        if (vowels.find(c) == string::npos) {
+            result += '.';
+            result += c;
+        }
+    }
+ 
+    cout << result << endl;
+ 
+    return 0;
+}
