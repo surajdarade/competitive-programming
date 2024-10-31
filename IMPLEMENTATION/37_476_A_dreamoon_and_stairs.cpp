efa48cbbb69884a619dc7dd5ac1075b4de450d03
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-31 20:59:33 +0530

    draemoon and stairs

---
+++ IMPLEMENTATION/37_476_A_dreamoon_and_stairs.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#define ll long long int
+#define cint \
+    int t;   \
+    cin >> t;
+#define br << endl;
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout << fixed << setprecision(n);
+#define For(i, n) for (ll i = 0; i < n; i++)
+#define Forran(i, n, a) for (ll i = n + 1; i <= a; i++)
+#define Forrevran(i, n, a) for (ll i = n - 1; i >= a; i--)
+#define Forrev(i, n) for (ll i = n - 1; i >= 0; i--)
+#define total(v) accumulate(v.begin(), v.end(), 0)
+#define diff(ax, ay, bx, by) abs(ax - bx) + abs(ay - by)
+#define distance(ax, ay, bx, by) pow((ax - bx) * (ax - bx) + (ay - by) * (ay - by), 0.5)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int main() {
+
+    fastio
+
+    int n, m;
+    
+    cin >> n >> m;
+    
+    if(n < m) {
+        cout << "-1";
+        return 0;
+    }
+    else {
+        int meta;
+        
+        if(n%2==0) meta=n/2;
+        else meta=(n/2)+1;
+        
+        if(meta%m==0) cout << meta << endl;
+        else {
+            while(meta%m!=0) {
+                meta++;
+            }
+            cout << meta << endl;
+        }
+    }
+
+    return 0;
+
+}
