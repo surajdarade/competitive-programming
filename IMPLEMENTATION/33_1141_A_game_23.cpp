cb31a95afde84ff34b8f989c321d7e5160829afc
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-31 20:57:18 +0530

    game 23

---
+++ IMPLEMENTATION/33_1141_A_game_23.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#define ll long long int
+#define cint \
+    int t;   \
+    cin >> t;
+#define br << endl;
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout << fixed << setprecision(n);
+#define For(i, n) for (ll i = 0; i < n; i++)
+#define Forran(i, n, a) for (ll i = n + 1; i <= a; i++)
+#define Forrevran(i, n, a) for (ll i = n - 1; i >= a; i--)
+#define Forrev(i, n) for (ll i = n - 1; i >= 0; i--)
+#define total(v) accumulate(v.begin(), v.end(), 0)
+#define diff(ax, ay, bx, by) abs(ax - bx) + abs(ay - by)
+#define distance(ax, ay, bx, by) pow((ax - bx) * (ax - bx) + (ay - by) * (ay - by), 0.5)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int main() {
+
+    fastio
+
+    int n, m;
+    cin >> n >> m;
+    
+    if(n == m) {
+        cout << "0" << endl;
+        return 0;
+    }
+    
+    if(m % n != 0) {
+        cout << "-1" << endl;
+        return 0;
+    }
+    
+    else if(m % n == 0) {
+        int res = 0;
+        
+        int d = m / n;
+        
+        while(d%2==0) d /= 2, res++;
+        while(d%3==0) d /= 3, res++;
+        
+        if(d!=1) res = -1;
+        cout << res << endl;
+    }
+
+    return 0;
+
+}
