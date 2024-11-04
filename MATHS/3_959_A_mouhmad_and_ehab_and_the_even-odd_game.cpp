1ec2fec2eaa27c83939d99c3ae611fa59135a558
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-11-04 21:30:54 +0530

    mouhmad and ehab and the even-odd game

---
+++ MATHS/3_959_A_mouhmad_and_ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#define ll long long int
+#define br << "\n";
+#define no "NO"
+#define yes "YES"
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout << fixed << setprecision(n);
+#define For(i, n) for (ll i = 0; i < n; i++)
+#define Forran(i, n, a) for (ll i = n + 1; i <= a; i++)
+#define Forrevran(i, n, a) for (ll i = n - 1; i >= a; i--)
+#define Forrev(i, n) for (ll i = n - 1; i >= 0; i--)
+#define total(v) accumulate(v.begin(), v.end(), 0)
+#define diff(ax, ay, bx, by) abs(ax - bx) + abs(ay - by)
+#define distance(ax, ay, bx, by) pow((ax - bx) * (ax - bx) + (ay - by) * (ay - by), 0.5)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int main() {
+
+    fastio
+
+    int n;
+    cin >> n;
+
+    if(n % 2 == 0) {
+        cout << "Mahmoud";
+    }else {
+        cout << "Ehab";
+    }
+
+    return 0;
+
+}
