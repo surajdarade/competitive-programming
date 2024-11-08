e1a4e345609722113fc6d5494abc2d54511bfc25
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-11-08 09:03:22 +0530

    dice tower

---
+++ MATHS/25_1266_B_dice_tower.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#define cint int t; cin >> t;
+#define ll long long int
+#define br << endl;
+#define no "NO"
+#define yes "YES"
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout<<fixed<<setprecision(n);
+#define For(i,n) for(ll i=0;i<n;i++)
+#define Forran(i,n,a) for(ll i=n+1;i<=a;i++)
+#define Forrevran(i,n,a) for(ll i=n-1;i>=a;i--)
+#define Forrev(i,n) for(ll i = n-1; i >= 0; i--)
+#define total(v) accumulate(v.begin(),v.end(),0)
+#define diff(ax,ay,bx,by) abs(ax-bx)+abs(ay-by)
+#define distance(ax,ay,bx,by) pow((ax-bx)*(ax-bx)+(ay-by)*(ay-by),0.5)
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main() {
+    fastio
+    cint
+    while (t--) {
+        long long x;
+        cin >> x;
+        if (x < 15) {
+            cout << "NO\n";
+            continue;
+        }
+        bool possible = false;
+        for (int top_face = 1; top_face <= 6; ++top_face) {
+            if ((x - top_face) % 14 == 0 && (x - top_face) / 14 >= 1) {
+                possible = true;
+                break;
+            }
+        }
+        cout << (possible ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
