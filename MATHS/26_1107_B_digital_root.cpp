5cc57b78e4a9c4fefb4e6c54a104762554f0aa60
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-11-08 09:03:52 +0530

    digital root

---
+++ MATHS/26_1107_B_digital_root.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#define cint int t; cin >> t;
+#define ll long long int
+#define br << endl;
+#define no "NO"
+#define yes "YES"
+#define vecint vector<int>
+#define vecll vector<long long int>
+#define vecstring vector<string>
+#define push push_back
+#define decimal(n) cout<<fixed<<setprecision(n);
+#define For(i,n) for(ll i=0;i<n;i++)
+#define Forran(i,n,a) for(ll i=n+1;i<=a;i++)
+#define Forrevran(i,n,a) for(ll i=n-1;i>=a;i--)
+#define Forrev(i,n) for(ll i = n-1; i >= 0; i--)
+#define total(v) accumulate(v.begin(),v.end(),0)
+#define diff(ax,ay,bx,by) abs(ax-bx)+abs(ay-by)
+#define distance(ax,ay,bx,by) pow((ax-bx)*(ax-bx)+(ay-by)*(ay-by),0.5)
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main() {
+    fastio
+    ll m,n,t,b,c,d,i,j,k,x,y,z,l,q,r;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin>>k>>x;
+        cout<<(x+9*(k-1) )<<endl;
+    }
+    return 0;
+}
